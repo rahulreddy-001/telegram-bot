830b54b8f831c9da698c598cee47ab5231392442
Rahul Reddy <rahulreddya5525@gmail.com>
2022-03-15 23:48:02 +0530

    edited docs

---
+++ foot.js
@@ -0,0 +1,3 @@
+const { Fotmob } = require("fotmob");
+const fotmob = Fotmob();
+fotmob.getMatchesByDate("20201020");
--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var express = require("express");
 const { Telegraf } = require("telegraf");
 const TOKEN = "5299987586:AAFnDDwZkwBDOIpLgO2GxCEYfHaltZfRYKY";
 const bot = new Telegraf(TOKEN);
-const { getMatchDetails } = require("./fotData");
+// const { getMatchDetails } = require("./fotData");
 const cors = require("cors");
 //________________________________________________________________________________________
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5003;
 app.get("/", (req, res) => {
   res.status(200).send("Home Page");
 });
@@ -83,6 +83,82 @@ bot.command("news", async (ctx) => {
 });
 
 //=================================================================
+const getData = async () => {
+  let date = new Date();
+  date = date.toLocaleDateString();
+  date = date.split("/")[2] + date.split("/")[1] + date.split("/")[0];
+  let response = await axios.get(`https://www.fotmob.com/matches?date=${date}`);
+
+  return response.data;
+};
+
+const getMatchData = async (matchId) => {
+  let response = await axios.get(
+    `https://www.fotmob.com/matchDetails?matchId=${matchId}`
+  );
+  return response.data;
+};
+
+const fetchDetails = async (matchId) => {
+  let matchData = await getMatchData(matchId);
+  let responseStr = "\n";
+  if (matchData.header.events !== null) {
+    if (matchData.header.events.homeTeamGoals !== null) {
+      let homeTeam = matchData.header.events.homeTeamGoals;
+      homeTeam = Object.values(homeTeam);
+      responseStr += `${matchData.general.homeTeam.name} :\n\n`;
+      if (homeTeam !== null) {
+        homeTeam.map((p) => {
+          p.map((g) => {
+            responseStr += `${g.nameStr}  ${g.timeStr} ${g.assistStr || ""}\n`;
+          });
+        });
+      }
+    }
+    if (matchData.header.events.awayTeamGoals !== null) {
+      let awayTeam = matchData.header.events.awayTeamGoals;
+      awayTeam = Object.values(awayTeam);
+      responseStr += `\n${matchData.general.awayTeam.name} :\n\n`;
+      if (awayTeam !== null) {
+        awayTeam.map((p) => {
+          p.map((g) => {
+            responseStr += `${g.nameStr}  ${g.timeStr} ${g.assistStr || ""}\n`;
+          });
+        });
+      }
+    }
+  }
+  return responseStr;
+};
+getMatchDetails = async (leg, bot, ctx) => {
+  let data = await getData();
+  let flag = 0;
+  data.leagues.map((e) => {
+    if (e.id === leg) {
+      flag += 1;
+      let match = e.matches;
+
+      match.map(async (m) => {
+        var responseValue = "";
+        let details = await fetchDetails(m.id);
+        responseValue += `Time      ::  ${m.time}\n`;
+        responseValue += `Home    ::  ${m.home.name}\n`;
+        responseValue += `Away     ::  ${m.away.name}\n`;
+        responseValue += `Score    ::  ${m.status.scoreStr}\n`;
+        responseValue += `Status   ::  ${m.status.reason.long}\n`;
+        responseValue += details;
+        bot.telegram.sendMessage(ctx.chat.id, responseValue);
+        //console.log(responseValue);
+      });
+    }
+  });
+  if (flag === 0) {
+    bot.telegram.sendMessage(
+      ctx.chat.id,
+      "Looks like no matches are sheduled today..."
+    );
+  }
+};
 
 bot.command("fotscore", (ctx) => {
   console.log(ctx.from);
